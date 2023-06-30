8ed54e34b1f381b639373a33b43784763aa961cf
ssvyatoss <fursin997@gmail.com>
2023-06-30 16:23:30 +0300

    add useLocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,22 @@ import { Form } from './Form/Form';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Filter } from './Filter/Filter';
 import { StyledGlobal } from 'GlobalStyle';
-import { saveData, loadData } from '../utils/localstorage';
+
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
+  const [contacts, setContacts] = useLocalStorage('contacts', [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -14,16 +26,16 @@ export const App = () => {
   ]);
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    const savedContacts = loadData('contacts');
-    if (savedContacts) {
-      setContacts(savedContacts);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const savedContacts = loadData('contacts');
+  //   if (savedContacts) {
+  //     setContacts(savedContacts);
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    saveData(contacts, 'contacts');
-  }, [contacts]);
+  // useEffect(() => {
+  //   saveData(contacts, 'contacts');
+  // }, [contacts]);
 
   const onSubmit = ({ name, number }) => {
     const isContactExist = contacts.some(
